709f8b6738a1f39c089705a5df53ae4604a8937c
Ajay Kidave <ajayvk@clace.io>
2024-10-29 09:27:55 -0700

    Change tabs to spaces

--- static/install.sh
+++ static/install.sh
@@ -7,34 +7,34 @@ set -e
 
 main() {
     LATEST_VERSION=$(curl -s https://api.github.com/repos/claceio/clace/releases/latest | grep tag_name | cut -d '"' -f 4)
-	os=$(uname -s | tr '[:upper:]' '[:lower:]')
-	arch=$(uname -m)
+    os=$(uname -s | tr '[:upper:]' '[:lower:]')
+    arch=$(uname -m)
 
     if test $arch = "x86_64"; then
         arch="amd64"
     fi
 
-	version=${1:-${LATEST_VERSION}}
+    version=${1:-${LATEST_VERSION}}
     target="clace-${version}-${os}-${arch}"
     clace_uri="https://github.com/claceio/clace/releases/download/$version/${target}.tar.gz"
-	clace_install="${CL_HOME:-$HOME/clhome}"
+    clace_install="${CL_HOME:-$HOME/clhome}"
 
-	bin_dir="$clace_install/bin"
-	tmp_dir="$clace_install/tmp"
-	exe="$bin_dir/clace"
+    bin_dir="$clace_install/bin"
+    tmp_dir="$clace_install/tmp"
+    exe="$bin_dir/clace"
 
-	mkdir -p "$bin_dir"
-	mkdir -p "$tmp_dir"
+    mkdir -p "$bin_dir"
+    mkdir -p "$tmp_dir"
 
-	curl -q --fail --location --progress-bar --output "$tmp_dir/clace.tar.gz" "$clace_uri" || (echo "Error downloading $clace_uri"; exit 1)
-	# extract to tmp dir so we don't open existing executable file for writing
-	tar -C "$tmp_dir" -xzf "$tmp_dir/clace.tar.gz"
-	chmod +x "$tmp_dir/${target}/clace"
-	mv "$tmp_dir/${target}/clace" "$exe"
-	rm "$tmp_dir/clace.tar.gz"
+    curl -q --fail --location --progress-bar --output "$tmp_dir/clace.tar.gz" "$clace_uri" || (echo "Error downloading $clace_uri"; exit 1)
+    # extract to tmp dir so we don't open existing executable file for writing
+    tar -C "$tmp_dir" -xzf "$tmp_dir/clace.tar.gz"
+    chmod +x "$tmp_dir/${target}/clace"
+    mv "$tmp_dir/${target}/clace" "$exe"
+    rm "$tmp_dir/clace.tar.gz"
 
     echo ""
-	echo "clace was installed successfully to $exe"
+    echo "clace was installed successfully to $exe"
 
     if test ! -s $clace_install/clace.toml; then
         echo ""
